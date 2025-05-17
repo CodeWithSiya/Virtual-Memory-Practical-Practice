a62387bdd25fa0d7683f7b522e5712d76a453287
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2025-05-17 18:50:30 +0000

    OPT - Initialised solution.

---
+++ src/opt-solution/OPT.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+//
+public class OPT 
+{
+    private static int optimal(final Memory frames, final Integer[] pageReferences) {
+        // Initialise the number of page faults.
+        int pageFaults = 0;
+        return pageFaults;
+    }
+
+    public static void main(final String[] args) {
+        final Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("Enter the physical memory size (number of frames):");
+        final int numFrames = stdIn.nextInt();
+        stdIn.nextLine();
+
+        System.out.println("Enter the string of page references:");
+        final String referenceString = stdIn.nextLine();
+
+        System.out.printf("Page faults: %d.\n", optimal(new Memory(numFrames), toArray(referenceString)));
+    }
+
+    private static Integer[] toArray(final String referenceString) {
+        final Integer[] result = new Integer[referenceString.length()];
+        
+        for(int i=0; i < referenceString.length(); i++) {
+            result[i] = Character.digit(referenceString.charAt(i), 10);
+        }
+        return result;
+    }
+}
