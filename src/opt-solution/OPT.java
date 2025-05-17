b5e587c13f5e22ffbef080eebceac76335b14306
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2025-05-17 22:34:06 +0000

    OPT - Implemented Initial Solution.

--- src/opt-solution/OPT.java
+++ src/opt-solution/OPT.java
@@ -5,7 +5,63 @@ public class OPT
     private static int optimal(final Memory frames, final Integer[] pageReferences) {
         // Initialise the number of page faults.
         int pageFaults = 0;
+
+        // Initialising the frame pointer.
+        int frame = 0;
+
+        // Looping through each page in the reference string.
+        for (int i = 0; i < pageReferences.length; i++) {
+            int page = pageReferences[i];
+
+            // Check if we have a page fault and an empty space available.
+            if (!frames.contains(page) && frames.isEmpty(frame)) {
+                // Add the page to memory, increment the number of page faults and frame pointer.
+                frames.put(frame, page); pageFaults++; frame = (frame + 1) % frames.size();
+                System.out.println(page + ": " + frames);
+            } else if (!frames.contains(page) && !frames.isEmpty(frame)) {
+                // Find and replace the victim page.
+                int victim = findVictim(i, frames, pageReferences);
+                frames.replace(victim, page); pageFaults++;
+                System.out.println(page + ": " + frames);
+            } 
+            else {
+                // No page fault.
+                System.out.println(page + ": " + "-"); 
+            }
+        }
         return pageFaults;
+    }
+
+    /**
+     * Helper function which finds the victim page from the given start point.
+     */
+    private static int findVictim(int startIndex, Memory frames, Integer[] pageReferences) {
+        // Initialise the victim page to the first frame and initialise the max until use count.
+        int victim = frames.get(0);
+        int maxUntilUse = -1;
+
+        // Loop through each frame and find the victim frame.
+        for (int i = 0; i < frames.size(); i++) {
+            int frame = frames.get(i);
+            int timeUntilUse = Integer.MAX_VALUE;
+
+            // Look into the future and find the victim page.
+            for (int j = startIndex + 1; j < pageReferences.length; j++) {
+                int page = pageReferences[j];
+                // Check if found first usage and stop the loop if found.
+                if (page == frame) {
+                    timeUntilUse = j - startIndex;
+                    break;
+                }
+            }
+            
+            // Check if this frame has the longest time until use and set it to victim if true.
+            if (timeUntilUse > maxUntilUse) {
+                maxUntilUse = timeUntilUse;
+                victim = frame;
+            }
+        }
+        return victim;
     }
 
     public static void main(final String[] args) {
