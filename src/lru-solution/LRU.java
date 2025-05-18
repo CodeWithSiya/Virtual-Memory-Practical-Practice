840e14505cf34f8a428f97878a614981f30ac228
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2025-05-18 08:59:01 +0000

    LRU - Implemented Initial Solution.

--- src/lru-solution/LRU.java
+++ src/lru-solution/LRU.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 //
 public class LRU 
@@ -5,6 +7,38 @@ public class LRU
     private static int leastRecentlyUsed(final Memory frames, final Integer[] pageReferences) {
         // Initialise the number of page faults and frame pointer.
         int pageFaults = 0;
+        int frame = 0;
+
+        // Initialise a stack for the LRU algorithm.
+        List<Integer> stack = new ArrayList<>();
+
+        // Loop through each page in page references.
+        for (int i = 0; i < pageReferences.length; i++) {
+            int page = pageReferences[i];
+
+            // Check if there is a page fault, add to memory and stack.
+            if (!frames.contains(page) && frames.isEmpty(frame)) {
+                // Add the page to memory, increment the number of page faults and pointer.
+                frames.put(frame, page); pageFaults++; frame = (frame + 1) % frames.size();
+                System.out.println(page + ": " + frames);
+            } else if (!frames.contains(page) && !frames.isEmpty(frame)) {
+                // There is a page fault, select a victim page from the stack and replace it.
+                int victim = stack.remove(stack.size() - 1);
+                frames.replace(victim, page); pageFaults++;
+                System.out.println(page + ": " + frames);
+            } else {
+                // No page fault.
+                System.out.println(page + ":-");
+            }
+            
+            // Check if the stack already contains the page, remove it if it does.
+            if (!stack.contains(page)) {
+                stack.add(0, page);
+            } else {
+                stack.remove((Integer)page);
+                stack.add(0, page);
+            }   
+        } 
         return pageFaults;
     }
 
